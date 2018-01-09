30109294446de824fe6d461d05e6bb4b5e606216
Jayme Ramsay <jayme.ramsay@gmail.com>
2018-01-09 14:39:29 -0600

    adds proxy and files to Express server so React can load

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser');
 const session = require('cookie-session');
 
 const users = require('./routes/users');
+const items = require('./routes/items');
 // const auth = require('./routes/auth');
 
 // http method middleware for PATCH and DELETE
@@ -24,7 +25,13 @@ app.use(cookieParser());
 
 // routes
 app.use('/users', users);
+app.use('/items', items);
 // app.use('/auth', auth);
+
+// adds react
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/src", express.static(__dirname + "/src"));
+
 
 // sets up initial page
 // app.get('/', (req, res, next) => {
